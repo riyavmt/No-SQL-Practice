70fef7019b79828bcf9244dcc72a0c50c591d1da
RIYA VERMA <riyavmt14@gmail.com>
2024-04-13 18:47:44 +0530

    Added Remove From Cart Functionality

--- models/user.js
+++ models/user.js
@@ -44,6 +44,13 @@ userSchema.methods.addToCart = function(product){
           console.log(err)
         })
 }
+userSchema.methods.removeFromCart = function(productId){
+const updatedCartItems = this.cart.items.filter(item=>{
+        return item.productId.toString() !== productId.toString()
+      })
+      this.cart.items =  updatedCartItems;
+      return this.save();
+}
 
 module.exports = mongoose.model("User",userSchema)
 
